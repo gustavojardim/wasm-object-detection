989eea3f4e986ea7e23ab8c684924f48b8b00068
Gustavo Jardim <gustavo.spadotto@gmail.com>
2026-01-10 18:41:09 -0300

    Add latency probe to be added to k8s nodes

---
+++ kubernetes/orchestration/latency-probe/latency_probe.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return {'timestamp': time.time()}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)from flask import Flask
